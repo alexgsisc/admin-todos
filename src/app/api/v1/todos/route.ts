c62a24e4ac2d79cbba99282679907e756ba33576
Alexis Guadarrama <alexgsisc@gmail.com>
2024-02-19 18:24:39 -0600

    send parameter

--- src/app/api/v1/todos/route.ts
+++ src/app/api/v1/todos/route.ts
@@ -2,7 +2,19 @@ import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req: Request) {
-  const todos = await prisma.todo.findMany();
+  // Get parameters from URL
+  const { searchParams } = new URL(req.url);
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take) || isNaN(skip)) {
+    return NextResponse.json({ error: "Invalid parameters" }, { status: 400 });
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+  });
 
   return NextResponse.json(todos);
 }
