83b10754f238dc885e39e5fce763f0a4bf3e598b
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-01 17:13:50 -0600

    add style

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Todo } from "@prisma/client";
-import style from "./TodoItem.module.css";
+import styles from "./TodoItem.module.css";
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface TodoItemProps {
@@ -10,13 +10,15 @@ interface TodoItemProps {
 
 export const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
   return (
-    <div className={todo.complete ? style.todoDone : style.todoPending}>
+    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div
-        onClick={() => toggleTodo(todo.id, !todo.complete)}
-          className={`flex p2 rounded-md cursor-pointer hover:bg-opacity-60 ${
-            todo.complete ? "bg-blue-100" : "bg-red-100"
-          }`}
+          onClick={() => toggleTodo(todo.id, !todo.complete)}
+          className={`
+            flex p-2 rounded-md cursor-pointer
+            hover:bg-opacity-60
+            ${todo.complete ? 'bg-blue-100' : 'bg-red-100'}
+          `}
         >
           {todo.complete ? (
             <IoCheckboxOutline size={30} />
--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -18,7 +18,7 @@ export const TodosGrid = ({ todos = [] }: TodosGridProps) => {
   };
 
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
       ))}
