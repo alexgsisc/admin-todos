cf2a8911dd3f1ce3d77925f83a06950028f0b189
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-12 18:40:23 -0600

    set cookies

--- src/Components/tabBar/TabBar.tsx
+++ src/Components/tabBar/TabBar.tsx
@@ -1,38 +1,43 @@
 // https://tailwindcomponents.com/component/radio-buttons-1
-import React from 'react'
+'use client'
+import { setCookie } from "cookies-next";
+import { useState } from "react";
 
-export const TabBar = () => {
+
+interface TabBarProps {
+    currentTab?: number;
+    tabOptions: number[];
+}
+
+export const TabBar = ({ tabOptions = [1, 2, 3, 4, 5], currentTab = 1 }: TabBarProps) => {
+
+    const [selectedTab, setSelectedTab] = useState(currentTab);
+
+    const onTabSelect = (tab: number) => {
+        setSelectedTab(tab)
+        setCookie('selectedTab', tab.toString(), { maxAge: 3600 * 12 * 1 })
+    }
+
     return (
-        <div className="grid w-full grid-cols-4 space-x-2 rounded-xl bg-gray-200 p-2">
+        <div className={`grid w-full ${'grid-cols-' + tabOptions.length} space-x-2 rounded-xl bg-gray-200 p-2`}>
+            {
+                tabOptions.map(tab => (
+                    <div key={tab}>
+                        <input
+                            checked={selectedTab === tab}
+                            onChange={() => { }}
+                            type="radio"
+                            id={tab.toString()}
+                            className="peer hidden" />
+                        <label
+                            onClick={() => onTabSelect(tab)}
+                            className="transition-all block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
+                            {tab}
+                        </label>
+                    </div>
 
-            <div>
-                <input type="radio" id="1" className="peer hidden" />
-                <label className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
-                    1
-                </label>
-            </div>
-
-            <div>
-                <input type="radio" id="2" className="peer hidden" />
-                <label className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
-                    2
-                </label>
-            </div>
-
-            <div>
-                <input type="radio" id="3" className="peer hidden" />
-                <label
-                    className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
-                    3
-                </label>
-            </div>
-
-            <div>
-                <input type="radio" id="4" className="peer hidden" />
-                <label className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">
-                    4
-                </label>
-            </div>
+                ))
+            }
         </div>
     )
 }
