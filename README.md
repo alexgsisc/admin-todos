99f304ced68fa2e9c75108ff2e7f1f836c1d1f2c
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-19 15:36:57 -0600

    update seed for creaste user and todos

--- README.md
+++ README.md
@@ -9,8 +9,14 @@ docker compose up -d
 
 2. rename file .env.template a .env
 3. update environment variables
-4. execute service Get seed for creating data base 
+4. Ejecutar el comando ```npm install``` para reconstruir los módulos de node
+5. Ejecutar el comando ```npm run dev``` para ejecutar aplicación en desarrollo
+6. execute service Get seed for creating data base 
 [Create Seed](localhost:3000/api/v1/seed)
+
+## Nota: Usuario por defecto
+__usuario:__  test1@example.com
+__password:__ test123
 
 # Prisma commands
 
@@ -25,6 +31,3 @@ npx prisma migrate dev
 ```
 npx prisma generate
 ```
-
-2.- You will execut seed 
-
--- src/app/api/v1/seed/route.ts
+++ src/app/api/v1/seed/route.ts
@@ -1,9 +1,31 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import bcrypt from 'bcryptjs';
+import { create } from "domain";
 
 export async function GET(request: Request) {
   //prepare insert
   await prisma.todo.deleteMany();
+  await prisma.user.deleteMany();
+
+  const users = await prisma.user.create({
+    data: {
+      email: "test1@example.com",
+      password: bcrypt.hashSync("test123"),
+      roles: ["admin", "user"],
+      todos: {
+        create: [
+          { description: 'Piedra del alma', complete: true },
+          { description: 'Piedra del poder' },
+          { description: 'Piedra del tiempo' },
+          { description: 'Piedra del espacio' },
+          { description: 'Piedra del realidad' },
+        ]
+      }
+    }
+  });
+
+
 
   //TODO insert a new todo
   // const todo = await prisma.todo.create({
@@ -15,23 +37,23 @@ export async function GET(request: Request) {
 
   //TODO create many todos
 
-  const todos = await prisma.todo.createMany({
-    data: [
-      {
-        description: "Data Fisrts",
-        complete: true,
-      },
-      {
-        description: "Data Second",
-      },
-      {
-        description: "Data Third",
-      },
-      {
-        description: "Data Fourth",
-      },
-    ],
-  });
+  // const todos = await prisma.todo.createMany({
+  //   data: [
+  //     {
+  //       description: "Data Fisrts",
+  //       complete: true,
+  //     },
+  //     {
+  //       description: "Data Second",
+  //     },
+  //     {
+  //       description: "Data Third",
+  //     },
+  //     {
+  //       description: "Data Fourth",
+  //     },
+  //   ],
+  // });
 
-  return NextResponse.json({ message: "Success", objects: todos });
+  return NextResponse.json({ message: "Success"});
 }
