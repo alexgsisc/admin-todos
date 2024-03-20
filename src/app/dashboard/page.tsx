a7f3afbb2d2c08b5e4fc9d19cf55b936e6e04259
Alexis Guadarrama <alexgsisc@gmail.com>
2024-03-19 18:06:49 -0600

    add format

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,35 +3,27 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
-
 export const metadata = {
   title: "Dashboard",
   description: "View Dashboard",
-}
+};
 
 export default async function DashboardPage() {
-
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    redirect('/api/auth/signin')
+    redirect("/api/auth/signin");
   }
-
 
   return (
     <div className="grid gap-6 grid-cols-1">
-
       <WidgetItem title="Usuario cpnectado S-Side">
         <span>{session.user?.name}</span>
         <span>{session.user?.image}</span>
         <span>{session.user?.email}</span>
 
-        <div>
-          {JSON.stringify(session)}
-        </div>
-
+        <div>{JSON.stringify(session)}</div>
       </WidgetItem>
-
     </div>
   );
 }
